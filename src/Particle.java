5a3261c0bc302953dc4c302900a37c2faecddefc
Sébastien BISSAY <seb.bissay@gmail.com>
2023-10-14 21:20:52 +0200

    Particle move method

--- src/Particle.java
+++ src/Particle.java
@@ -1,9 +1,14 @@
 public class Particle {
     protected float x, y, heading;
-    public static float sensorAngle, sensorDistance, rotationAngle, deposition;
+    public static float sensorAngle, sensorDistance, rotationAngle, deposition, stepSize;
 
     public Particle(float x, float y){
         this.x = x;
         this.y = y;
+    }
+
+    void move() {
+        x += stepSize * Math.cos(sensorAngle);
+        y += stepSize * Math.sin(sensorAngle);
     }
 }
