c996140828cc50f28a9a0f2d3ed0ee3c9e645294
Sébastien BISSAY <seb.bissay@gmail.com>
2023-10-15 18:19:39 +0200

    Pheromones detection method

--- src/Layer.java
+++ src/Layer.java
@@ -7,4 +7,18 @@ public class Layer {
         this.height = height;
         grid = new float[width][height];
     }
+
+    public float getPheromones(float x, float y) {
+        int intX = (int) Math.floor(x);
+        int intY = (int) Math.floor(y);
+        // Out of bounds, no trail
+        if (intX < 0 || intX > width - 1 || intY < 0 || intY > height - 1) return 0;
+        return
+                (1 - y + intY) *
+                        ((1 - x + intX) * grid[intX][intY]
+                        + (x - intX) * grid[intX + 1][intY])
+                + (y - intY) *
+                        ((1 - x + intX) * grid[intX][intY + 1]
+                                + (x - intX) * grid[intX + 1][intY + 1]);
+    }
 }
